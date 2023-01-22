b7152a6e7df435e2ce8cd87f10718b98bf4d1cb6
Sanjay <sanjaym1998@gmail.com>
2023-01-22 22:27:11 +0530

    Nodejs Assignment-3

---
+++ index.js
@@ -0,0 +1,14 @@
+const http =require('http');
+const fs =require('fs')
+const server =http.createServer((req,res)=>{
+    fs.readFile("index.html",(err,data)=>{
+        res.end(data)
+    })
+  
+   
+})
+server.listen(5000,()=>{
+   console.log("server is up");
+   
+})
+
